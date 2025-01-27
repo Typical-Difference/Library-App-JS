cfda89cbb046d588261d7cc4821e366db1e61787
Balaji S <104999488+Typical-Difference@users.noreply.github.com>
2025-01-27 22:17:39 +0530

    Version 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Library-App-JS
 Library app that's part of The Odin Project.
+
+This project covers topics: object prototyping and object constructors in js.
--- script.js
+++ script.js
@@ -2,13 +2,25 @@
 const library = [];
 
 //Constructor for books
-function Book(title, author){
+function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 //function to create a book and store in array
-function addToLibrary(title, author){
+function addToLibrary(title, author, pages, read){
     const newBook = new Book(title, author);
     library.add(newBook);
+}
+
+//loop through the array and display each book
+function displayAll(library){
+    for(let serialno = 1; serialno <= library.length; serialno++){
+        let authorname = library[serialno].author;
+        let booktitle = library[serialno].title;
+        let bookpages = library[serialno].pages;
+        let bookread = library[serialno].read;
+    }
 }
